a1bd63054faa3abc2b5e2bbd85dd865382a71a6c
Martin Orrego <martinorregosilva@uc.cl>
2022-08-17 19:49:40 -0400

    feat: add top days function

--- functions.py
+++ functions.py
@@ -36,7 +36,19 @@ def get_users_with_most_tweets(tweets, n):
 
 
 def get_days_with_most_tweets(tweets, n):
-    pass
+    days = defaultdict(int)
+    for tweet in tweets:
+        # format date is YYYY-MM-DDTHH:MM:SS+00:00
+        # only need YYYY-MM-DD -> 10 chars
+        days[tweet['date'][:10]] += 1
+
+    top = sorted(
+        days.items(),
+        key=lambda day: day[1],
+        reverse=True
+    )
+
+    return top[:n]
 
 
 def get_top_hashtags(tweets, n):
