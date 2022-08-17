daf01f21f3796fe729c533f9688879230cd283e2
Martin Orrego <martinorregosilva@uc.cl>
2022-08-17 19:24:19 -0400

    feat: add top retweeted function

--- functions.py
+++ functions.py
@@ -2,20 +2,30 @@ import json
 
 
 def load_tweets(filename):
+    with open(filename, 'r') as file:
+        tweets = [
+            json.loads(line) for line in file
+        ]
+
+    return tweets
+
+
+def get_most_tweets_retweeted(tweets, n):
+    retweeted = sorted(
+        tweets,
+        key=lambda tweet: tweet['retweetCount']
+    )
+
+    return retweeted[-n:]
+
+
+def get_users_with_most_tweets(tweets, n):
     pass
 
 
-def get_most_tweets_retweeted(file, n):
+def get_days_with_most_tweets(tweets, n):
     pass
 
 
-def get_users_with_most_tweets(file, n):
-    pass
-
-
-def get_days_with_most_tweets(file, n):
-    pass
-
-
-def get_top_hashtags(file, n):
+def get_top_hashtags(tweets, n):
     pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functions import (
+    load_tweets,
     get_most_tweets_retweeted,
     get_users_with_most_tweets,
     get_days_with_most_tweets,
@@ -6,8 +7,20 @@ from functions import (
 )
 
 
+FILENAME = 'farmers-protest-tweets-2021-03-5.json'
+
+
 def main():
-    pass
+
+    tweets = load_tweets(FILENAME)
+
+    top_retweeted = get_most_tweets_retweeted(tweets, 10)
+
+    print('Top retweeted:')
+    for tweet in top_retweeted:
+        print(tweet['content'])
+
+    print()
 
 
 if __name__ == '__main__':
