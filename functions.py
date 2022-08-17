ffc4a31549d581c0a0ed3a86210d9fd3349d269d
Martin Orrego <martinorregosilva@uc.cl>
2022-08-17 19:10:14 -0400

    feat: add base code

---
+++ functions.py
@@ -0,0 +1,21 @@
+import json
+
+
+def load_tweets(filename):
+    pass
+
+
+def get_most_tweets_retweeted(file, n):
+    pass
+
+
+def get_users_with_most_tweets(file, n):
+    pass
+
+
+def get_days_with_most_tweets(file, n):
+    pass
+
+
+def get_top_hashtags(file, n):
+    pass
---
+++ main.py
@@ -0,0 +1,14 @@
+from functions import (
+    get_most_tweets_retweeted,
+    get_users_with_most_tweets,
+    get_days_with_most_tweets,
+    get_top_hashtags,
+)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
